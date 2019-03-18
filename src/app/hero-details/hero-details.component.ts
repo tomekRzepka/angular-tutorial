f1a90bf94edb78c18151fcfa44fd172a1c60fa8b
Tomasz Rzepka <tomasz.rzepka@santander.pl>
2019-03-18 10:34:28 +0100

    modify hero-details

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from '../heroes/hero';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {HeroService} from '../heroes/hero.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -11,10 +14,22 @@ export class HeroDetailsComponent implements OnInit {
   @Input()
   hero: Hero;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) {
   }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,10 +19,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroServics.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
